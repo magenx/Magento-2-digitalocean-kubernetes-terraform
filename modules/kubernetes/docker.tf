670b501dd2fa3b994b17a86bb61365cd7a06afd1
MagenX <magenx@users.noreply.github.com>
2022-06-06 11:45:22 +0100

    Create docker.tf

---
+++ modules/kubernetes/docker.tf
@@ -0,0 +1,45 @@
+
+
+
+/////////////////////////////////////////////////////[ DOCKER BUILD AND PUSH ]////////////////////////////////////////////
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Build docker image per config
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "docker_image" "this" {
+  for_each     = var.docker_image
+  name         = each.key
+  force_remove = true
+  build {
+    path       = "${path.module}/docker/${each.key}"
+    tag        = ["${var.dockerhub}/${each.key}: "]
+    no_cache   = true
+    
+    build_arg  = {
+      var.docker_image.build_args[each.key]
+    }
+    
+    label      = {
+      author : var.brand
+    }
+    
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Push docker image to registry if updated
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "null_resource" "docker_push" {
+  for_each = var.docker_image
+  
+  triggers = {
+    docker = docker_image.this[each.key].latest
+  }
+
+  provisioner "local-exec" {
+    interpreter = ["bash", "-c"]
+    command = <<-EOT
+        echo "${DOCKERHUB_PASSWORD}" | docker login --username "${DOCKERHUB_USERNAME}" --password-stdin
+        docker push ${var.dockerhub}/${each.key}:${sum([ , 1])}
+      EOT
+  }
+}
+
