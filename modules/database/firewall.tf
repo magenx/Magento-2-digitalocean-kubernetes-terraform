40726a64b2eecb92880d60f3a8e6554b9294eaae
MagenX <magenx@users.noreply.github.com>
2022-06-08 21:09:18 +0100

    Create firewall.tf

---
+++ modules/database/firewall.tf
@@ -0,0 +1,17 @@
+
+
+
+////////////////////////////////////////////////////[ DATABASE TRUSTED SOURCES ]//////////////////////////////////////////
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create managed database services [ mysql | redis ] trusted sources firewall
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "digitalocean_database_firewall" "this" 
+  for_each   = digitalocean_database_cluster.this.id
+  cluster_id = each.key
+
+  rule {
+    type  = "tag"
+    value = "database"
+  }
+}
