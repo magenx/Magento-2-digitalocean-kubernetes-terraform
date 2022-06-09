c6b4d0360388ffb2bfeedea51ce2f19173876e38
MagenX <magenx@users.noreply.github.com>
2022-06-09 12:11:33 +0100

    Update firewall.tf

--- modules/database/firewall.tf
+++ modules/database/firewall.tf
@@ -7,8 +7,8 @@
 # Create managed database services [ mysql | redis ] trusted sources firewall
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "digitalocean_database_firewall" "this" 
-  for_each   = digitalocean_database_cluster.this.id
-  cluster_id = each.key
+  for_each   = digitalocean_database_cluster.this
+  cluster_id = each.value.id
 
   rule {
     type  = "tag"
