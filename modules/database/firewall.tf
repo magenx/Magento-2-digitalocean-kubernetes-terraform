e4ceb77573303d2dcfca97f50d40215ebcb37ec9
MagenX <magenx@users.noreply.github.com>
2022-06-10 10:06:48 +0100

    Update firewall.tf

--- modules/database/firewall.tf
+++ modules/database/firewall.tf
@@ -6,7 +6,7 @@
 # # ---------------------------------------------------------------------------------------------------------------------#
 # Create managed database services [ mysql | redis ] trusted sources firewall
 # # ---------------------------------------------------------------------------------------------------------------------#
-resource "digitalocean_database_firewall" "this" 
+resource "digitalocean_database_firewall" "this" {
   for_each   = digitalocean_database_cluster.this
   cluster_id = each.value.id
 
