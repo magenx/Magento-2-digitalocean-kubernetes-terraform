8e827683666b4b4c9d7df358f081dd91f1427f0f
MagenX <magenx@users.noreply.github.com>
2022-06-17 12:29:31 +0100

    Create helm_release_nfs.tf

---
+++ modules/kubernetes/helm_release_nfs.tf
@@ -0,0 +1,51 @@
+
+
+
+////////////////////////////////////////////////////////[ NFS PROVISIONER ]///////////////////////////////////////////////
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create nfs cluster provisioner
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "helm_release" "nfs" {
+  name       = "${var.project.name}-nfs"
+  repository = "https://openebs.github.io/charts"
+  chart      = "openebs"
+  set {
+    name  = "nfs-provisioner.enabled"
+    value = "true"
+  }
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create nfs storage class
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "kubernetes_storage_class" "nfs" {
+  metadata {
+    name        = "openebs-rwx"
+    annotations = {
+      "openebs.io/cas-type" = "nfsrwx"
+      "cas.openebs.io/config" = {
+        "NFSServerType" = "kernel"
+        "BackendStorageClass" = "openebs-hostpath"
+    }
+  }
+  storage_provisioner = "openebs.io/nfsrwx"
+  reclaim_policy      = "Retain"
+}
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create nfs volume claim
+# # ---------------------------------------------------------------------------------------------------------------------#
+resource "kubernetes_persistent_volume_claim" "nfs" {
+  metadata {
+    name = "${var.project.name}-nfs"
+  }
+  spec {
+    access_modes      = ["ReadWriteMany"]
+    storage_class_nme = "openebs-rwx"
+    resources {
+      requests = {
+        storage = "100G"
+      }
+    }
+  }
+}
+  
